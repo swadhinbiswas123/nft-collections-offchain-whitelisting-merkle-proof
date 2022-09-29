59f72079050e1bef57535e04d6dea961a4411efa
Swadhin Biswas <swadhincpa@gmail.com>
2022-09-29 19:27:40 +0600

    Importing merkle openzeppline

--- Smart_Contract/contracts/nftmerkle.sol
+++ Smart_Contract/contracts/nftmerkle.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MftMerkle is ERC721, ERC721Burnable, Ownable {
     constructor() ERC721("Nft Merkle", "NMK") {}
