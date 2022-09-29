8a57c926fe5dda90f234139ffc2c6efc1a1b5e83
Swadhin Biswas <swadhincpa@gmail.com>
2022-09-29 19:01:43 +0600

    Adding merkle tree from openzeppline

--- Smart_Contract/contracts/nftmerkle.sol
+++ Smart_Contract/contracts/nftmerkle.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MftMerkle is ERC721, ERC721Burnable, Ownable {
     constructor() ERC721("Nft Merkle", "NMK") {}
@@ -11,6 +12,8 @@ contract MftMerkle is ERC721, ERC721Burnable, Ownable {
     function _baseURI() internal pure override returns (string memory) {
         return "baseuri.com";
     }
+
+
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
