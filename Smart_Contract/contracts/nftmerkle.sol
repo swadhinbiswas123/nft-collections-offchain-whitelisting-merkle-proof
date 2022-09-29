88578e90788c248d04f000a4e3678f2eca9386b2
Swadhin Biswas <swadhincpa@gmail.com>
2022-09-29 20:47:02 +0600

    Changes on frontend

--- Smart_Contract/contracts/nftmerkle.sol
+++ Smart_Contract/contracts/nftmerkle.sol
@@ -7,17 +7,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MftMerkle is ERC721, ERC721Burnable, Ownable {
-    constructor() ERC721("Nft Merkle", "NMK") {}
+    bytes32 root;
+
+    constructor(bytes32 _root) ERC721("Nft Merkle", "NMK") {
+        root = _root;
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "baseuri.com";
     }
 
-    function isWhitelisted() public view returns(bool){
-        
+    function isWhitelisted(bytes32[] memory proof, bytes32 leaf) public view returns(bool){
+       return MerkleProof.verify(proof, root, leaf);
     }
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
+    function changeRoot(bytes32 _newRoot)  public onlyOwner{
+        root = _newRoot;
+    }
+
+    function safeMint(address to, uint256 tokenId, bytes32[] memory proof, bytes32 leaf) public onlyOwner {
+        require(isWhitelisted(proof, leaf), "Not a whitelisted member");
         _safeMint(to, tokenId);
     }
 }
