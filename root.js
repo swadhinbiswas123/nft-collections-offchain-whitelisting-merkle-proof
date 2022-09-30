eb7261f04c83c11db9760b54973281d49c4ad34a
Swadhin Biswas <swadhincpa@gmail.com>
2022-09-30 12:41:34 +0600

    Changing the whitelist addresses

--- root.js
+++ root.js
@@ -1,7 +1,7 @@
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
-const addresses = ["0xAA441bEAcB48f7d2a62374aBbd2154cB4C22c84a","0xC3032725b2b46bE569B03925484C80D593c88443", "0x728F2B96D15BDaa88368a371B4BD77CeA0083166", "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"];
+const addresses = ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"];
 
 
 const leaves = addresses.map(x =>keccak256(x));
